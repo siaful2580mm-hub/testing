d14f586e837796d7253968484730cac76c584864
siaful2580mm-hub <siaful2580mm@gmail.com>
2026-02-22 16:18:49 +0600

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 
-    document.addEventListener("DOMContentLoaded", function() {
-    
+
     // --- Mobile Hamburger Menu Toggle ---
     const hamburger = document.getElementById('hamburger');
     const navLinks = document.getElementById('nav-links');
